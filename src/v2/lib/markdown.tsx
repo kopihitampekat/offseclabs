0e998b556b71a455ff95dee3b73a8d63ea064016
kopihitampekat <muhammadhasan072@gmail.com>
2026-04-27 11:53:27 +0700

    fix markdown

--- src/v2/lib/markdown.tsx
+++ src/v2/lib/markdown.tsx
@@ -4,14 +4,7 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
 import type { ComponentPropsWithoutRef } from "react";
-
-const prettyCodeOptions = {
-  theme: "github-dark-default",
-  keepBackground: true,
-  defaultLang: "plaintext",
-};
 
 type MarkdownContentProps = Omit<
   ComponentPropsWithoutRef<typeof Markdown>,
@@ -27,15 +20,7 @@ export function MarkdownContent({ children, ...props }: MarkdownContentProps) {
       rehypePlugins={[
         rehypeSlug,
         [rehypeAutolinkHeadings, { behavior: "wrap" }],
-        [rehypePrettyCode, prettyCodeOptions],
       ]}
-      components={{
-        pre: ({ children, ...preProps }) => (
-          <div className="relative group">
-            <pre {...preProps}>{children}</pre>
-          </div>
-        ),
-      }}
       {...props}
     />
   );
